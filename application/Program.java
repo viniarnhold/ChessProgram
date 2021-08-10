f6282b1a8e16d44ca9dff5e4e764fc1cfb329693
viniarnhold <viniciusarnh@gmail.com>
2021-08-10 08:49:18 -0300

    Inicio da implementação do tabuleiro e da peça
    Close #3

--- application/Program.java
+++ application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
@@ -7,9 +8,9 @@ public class Program {
 
         System.out.println("Hello World!");
 
-        Position pos = new Position(3, 5);
+        Board board = new Board(8, 8);
 
-        System.out.println(pos);
+        System.out.println(board);
 
     }
 }
